973d621177d20f5bd53058eb20d188ae66461c55
Paweł Wojdas <paw.wojdas@gmail.com>
2020-07-02 21:03:34 +1000

    Updates

---
+++ precache-manifest.86ca92083d83a7c435743197afbeed75.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9324eccc1ecf7f5edd45bb9b9c7f89ca",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "16b39ce06aa68be6ce31",
+    "url": "/weather-app/static/css/main.dc080ea4.chunk.css"
+  },
+  {
+    "revision": "90e45767320fccf39c48",
+    "url": "/weather-app/static/js/2.4350f6ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.4350f6ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16b39ce06aa68be6ce31",
+    "url": "/weather-app/static/js/main.a3090ed3.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "2478683111e2fe2e7760ee1f6e72aaf8",
+    "url": "/weather-app/static/media/01d.24786831.png"
+  },
+  {
+    "revision": "7b9516422d09c59d11d0372999326c8b",
+    "url": "/weather-app/static/media/01n.7b951642.png"
+  },
+  {
+    "revision": "5153a3d19331f68a82f59e36f1f66fb6",
+    "url": "/weather-app/static/media/02d.5153a3d1.png"
+  },
+  {
+    "revision": "0460cd547c9da6fc83204e44f711926b",
+    "url": "/weather-app/static/media/02n.0460cd54.png"
+  },
+  {
+    "revision": "8ea5db03a55964b21fdd8a25ab7cd3a7",
+    "url": "/weather-app/static/media/05.8ea5db03.png"
+  },
+  {
+    "revision": "5737635c1e63c7ecc92e949b74686f78",
+    "url": "/weather-app/static/media/09d.5737635c.png"
+  },
+  {
+    "revision": "1e81d1c900159197d14d677f495da011",
+    "url": "/weather-app/static/media/10d.1e81d1c9.png"
+  },
+  {
+    "revision": "1e81d1c900159197d14d677f495da011",
+    "url": "/weather-app/static/media/10n.1e81d1c9.png"
+  },
+  {
+    "revision": "ff74d8976e9ac956a07d3431a600474f",
+    "url": "/weather-app/static/media/11d.ff74d897.png"
+  },
+  {
+    "revision": "c7a83e0e90d574416f76d17440ff991f",
+    "url": "/weather-app/static/media/13d.c7a83e0e.png"
+  },
+  {
+    "revision": "f6bfd5cc5fbe85742b2e0c79e9d5b78f",
+    "url": "/weather-app/static/media/50d.f6bfd5cc.png"
+  }
+]);
